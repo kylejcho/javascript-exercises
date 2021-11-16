7d1a3425ed2c5373a2f6a31bf24d2a3da6e6b135
Kyle Cho <kylejcho@gmail.com>
2021-11-16 15:53:17 -0500

    JS

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a , b) {
+    return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+    return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(sum) {
+    return sum.reduce((total, num) => total + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+    return array.reduce((total, num) => total * num , 1);
 };
 
-const power = function() {
-	
+const power = function(a , b) {
+    return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+    if (num === 0) return 1;
+    let product = 1;
+    for (let i = num; i > 0; i--) {
+        product *= i;
+    }
+    return product;
 };
 
 // Do not edit below this line
