d1fb623235af400da65c6a6a95dc321261a80aec
Kyle Cho <kylejcho@gmail.com>
2021-11-16 16:59:21 -0500

    js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function(str) {
+    let newStr = str.toLowerCase().replace(/[^a-z]/g, '');
+    return newStr === newStr.split('').reverse().join('');
 };
 
 // Do not edit below this line
